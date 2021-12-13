492430fd30ba86d81c8ff41228c0793f6ac080ba
Felipe Amaral <felipe.desenvolvimento@plasoft.com.br>
2021-12-12 21:28:30 -0300

    refactor: removido a função de sanitizar o cep devido a não utilização
    Como foi utilizado o express-validators para tratar todos os parâmetros fornecidos nas rotas, essa
    parte da sanitização foi removida, juntamente com seus testes

--- src/helpers/zipcodeSanitize/zipcodeSanitize.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Função que limpa os caracteres especiais e caracteres indevidos do CEP
- * @param inputZipcodeString string string do cep, formatado ou não
- * @returns a string limpa sem caracteres especiais.
- */
-
-export function zipcodeSanitize(inputZipcodeString: string): string {
-  return inputZipcodeString.trim().replace(/\D/gim, '');
-}
