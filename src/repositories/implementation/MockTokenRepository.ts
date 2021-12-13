501ec8795b9c6656554d55c256041856222db157
Felipe Amaral <felipe.desenvolvimento@plasoft.com.br>
2021-12-12 21:51:07 -0300

    test: implementado um teste para geração do token

--- src/repositories/implementation/MockTokenRepository.ts
+++ src/repositories/implementation/MockTokenRepository.ts
@@ -11,11 +11,11 @@ export class MockTokenRepository implements ITokenRepository {
   private verifyToken = verify;
 
   async generateToken(userId: string): Promise<string> {
-    return this.signToken({ id: userId }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+    return this.signToken({ id: userId }, process.env.TOKEN_SECRET || 'testSecretKey', { expiresIn: '1h' });
   }
 
   async validateToken(token: string): Promise<Boolean> {
-    const tokenDecoded = this.verifyToken(token, process.env.TOKEN_SECRET);
+    const tokenDecoded = this.verifyToken(token, process.env.TOKEN_SECRET || 'testSecretKey');
     if (tokenDecoded) {
       return true;
     }
---
+++ src/usecases/GenerateToken/GenerateToken.spec.ts
@@ -0,0 +1,12 @@
+import { MockTokenRepository } from '@repositories/implementation/MockTokenRepository';
+import { GenerateTokenController } from './GenerateTokenController';
+import { GenerateTokenUseCase } from './GenerateTokenUseCase';
+
+describe('GenerateToken test functions', () => {
+  const mockTokenRepository = new MockTokenRepository();
+  const generateTokenUseCase = new GenerateTokenUseCase(mockTokenRepository);
+
+  it('Should generate a valid token', async () => {
+    expect(typeof (await generateTokenUseCase.execute({ id: '1111-2222-333' }))).toBe('string');
+  });
+});
